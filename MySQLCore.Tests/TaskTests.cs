cba7ef385b3ab00723a6bd0c27c25eafbac82e6c
John Franti <jfranti@gmail.com>
2017-08-02 21:39:05 -0700

    refactor, remove console readouts

--- MySQLCore.Tests/TaskTests.cs
+++ MySQLCore.Tests/TaskTests.cs
@@ -9,37 +9,30 @@ namespace MySQLCore.Tests
     [TestClass]
     public class TaskTests : IDisposable
     {
-
         public void Dispose()
         {
             Task.DeleteAll();
         }
-
         public TaskTests()
         {
             DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=3306;database=mysqlcore_test;";
         }
-
         [TestMethod]
         public void VerifyName_True()
         {
             //Arrange
             Task newTask = new Task("Walk the dog");
-
             //Assert
             Assert.AreEqual("Walk the dog", newTask.GetName());
         }
-
         [TestMethod]
         public void VerifyName_False()
         {
             //Arrange
             Task newTask = new Task("Mow the lawn");
-
             //Assert
             Assert.AreNotEqual("Walk the dog", newTask.GetName());
         }
-
         [TestMethod]
         public void SaveToDatabase_True()
         {
@@ -50,12 +43,6 @@ namespace MySQLCore.Tests
             arrangedList.Add(newTask);
             // Act
             List<Task> allTasks = Task.GetAll();
-
-            Console.Write("the id from the arranged list is ");
-            Console.WriteLine(arrangedList[0].GetId());
-            Console.Write("the id from the recovred list is ");
-            Console.WriteLine(allTasks[0].GetId());
-
             // Assert
             CollectionAssert.AreEqual(arrangedList, allTasks);
         }
--- MySQLCore/Models/Task.cs
+++ MySQLCore/Models/Task.cs
@@ -8,23 +8,19 @@ namespace MySQLCore.Models
     {
         private string _name;
         private int _id;
-
         public Task(string name, int id = 0)
         {
             _name = name;
             _id = id;
         }
-
         public string GetName()
         {
             return _name;
         }
-
         public int GetId()
         {
             return _id;
         }
-
         public void Save()
         {
             MySqlConnection conn = DB.Connection();
@@ -40,49 +36,32 @@ namespace MySQLCore.Models
 
             cmd.ExecuteNonQuery();
             _id = (int) cmd.LastInsertedId;
-
-            Console.Write("Id inserted is ");
-            Console.WriteLine(cmd.LastInsertedId);
-            Console.Write("Id assigned is ");
-            Console.WriteLine(_id);
-
         }
-
         public static List<Task> GetAll()
         {
             List<Task> allTasks = new List<Task> {};
-
             MySqlConnection conn = DB.Connection();
             conn.Open();
-
             var cmd = conn.CreateCommand() as MySqlCommand;
             cmd.CommandText = @"SELECT * FROM tasks;";
-
             var rdr = cmd.ExecuteReader() as MySqlDataReader;
-
             while(rdr.Read())
             {
               int taskId = rdr.GetInt32(0);
               string taskName = rdr.GetString(1);
               Task newTask = new Task(taskName, taskId);
-              Console.Write("Recovered task id is ");
-              Console.WriteLine(taskId);
               allTasks.Add(newTask);
             }
             return allTasks;
         }
-
         public static void DeleteAll()
         {
             MySqlConnection conn = DB.Connection();
             conn.Open();
-
             var cmd = conn.CreateCommand() as MySqlCommand;
             cmd.CommandText = @"DELETE FROM tasks;";
             cmd.ExecuteNonQuery();
-
         }
-
         public override bool Equals(System.Object otherTask)
         {
           if (!(otherTask is Task))
@@ -95,12 +74,9 @@ namespace MySQLCore.Models
             return this.GetId().Equals(newTask.GetId());
           }
         }
-
         public override int GetHashCode()
         {
              return this.GetId().GetHashCode();
         }
-
-
     }
 }
