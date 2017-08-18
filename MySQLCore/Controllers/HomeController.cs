b0cd88fb581c5189a3e2b5ca25ff7f97c5834d4d
John Franti <jfranti@gmail.com>
2017-08-18 15:25:00 -0700

    updated with main lesson Wk4

--- MySQLCore/Controllers/HomeController.cs
+++ MySQLCore/Controllers/HomeController.cs
@@ -8,70 +8,100 @@ namespace MySQLCore.Controllers
     public class HomeController : Controller
     {
 
-// HOME AND CREATE ROUTES
-
+// HOME AND MENU ROUTES
         [HttpGet("/")]
         public ActionResult Index()
         {
+            return View();
+        }
+        [HttpGet("/tasks")]
+        public ActionResult Tasks()
+        {
             List<Task> allTasks = Task.GetAll();
+            return View(allTasks);
+        }
+        [HttpGet("/categories")]
+        public ActionResult Categories()
+        {
             List<Category> allCategories = Category.GetAll();
-            Dictionary<string, object> model = new Dictionary<string, object>() {};
-            model.Add("tasks", allTasks);
-            model.Add("categories", allCategories);
+            return View(allCategories);
+        }
+
+//NEW TASK
+        [HttpGet("/tasks/new")]
+        public ActionResult TaskForm()
+        {
+            return View();
+        }
+        [HttpPost("/tasks/new")]
+        public ActionResult TaskCreate()
+        {
+            Task newTask = new Task(Request.Form["task-description"]);
+            newTask.Save();
+            return View("Success");
+        }
+
+//NEW CATEGORY
+        [HttpGet("/categories/new")]
+        public ActionResult CategoryForm()
+        {
+            return View();
+        }
+        [HttpPost("/categories/new")]
+        public ActionResult CategoryCreate()
+        {
+            Category newCategory = new Category(Request.Form["category-name"]);
+            newCategory.Save();
+            return View("Success");
+        }
+
+//ONE TASK
+        [HttpGet("/tasks/{id}")]
+        public ActionResult TaskDetail(int id)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Task selectedTask = Task.Find(id);
+            List<Category> TaskCategories = selectedTask.GetCategories();
+            List<Category> AllCategories = Category.GetAll();
+            model.Add("task", selectedTask);
+            model.Add("taskCategories", TaskCategories);
+            model.Add("allCategories", AllCategories);
+            return View( model);
+
+        }
+
+//ONE CATEGORY
+        [HttpGet("/categories/{id}")]
+        public ActionResult CategoryDetail(int id)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Category SelectedCategory = Category.Find(id);
+            List<Task> CategoryTasks = SelectedCategory.GetTasks();
+            List<Task> AllTasks = Task.GetAll();
+            model.Add("category", SelectedCategory);
+            model.Add("categoryTasks", CategoryTasks);
+            model.Add("allTasks", AllTasks);
             return View(model);
         }
 
-        [HttpPost("/tasks/add")]
-        public ActionResult CreateTask()
+//ADD CATEGORY TO TASK
+        [HttpPost("task/add_category")]
+        public ActionResult TaskAddCategory()
         {
-            Task newTask = new Task(Request.Form["taskname"], Int32.Parse(Request.Form["taskcategory"]));
-            newTask.Save();
-            return RedirectToAction("Index");
+            Category category = Category.Find(Int32.Parse(Request.Form["category-id"]));
+            Task task = Task.Find(Int32.Parse(Request.Form["task-id"]));
+            task.AddCategory(category);
+            return View("Success");
         }
 
-        [HttpPost("/category/add")]
-        public ActionResult CreateCategory()
+//ADD TASK TO CATEGORY
+        [HttpPost("category/add_task")]
+        public ActionResult CategoryAddTask()
         {
-            Category newCategory = new Category(Request.Form["categoryname"]);
-            newCategory.Save();
-            return RedirectToAction("Index");
+            Category category = Category.Find(Int32.Parse(Request.Form["category-id"]));
+            Task task = Task.Find(Int32.Parse(Request.Form["task-id"]));
+            category.AddTask(task);
+            return View("Success");
         }
-
-
-        //EDIT ROUTES
-        [HttpGet("/tasks/{id}/edit")]
-        public ActionResult EditTask(int id)
-        {
-            Task thisTask = Task.Find(id);
-            return View(thisTask);
-        }
-
-        [HttpPost("/tasks/{id}/edit")]
-        public ActionResult EditTaskConfirm(int id)
-        {
-            Task thisTask = Task.Find(id);
-            thisTask.UpdateDescription(Request.Form["newname"]);
-            return RedirectToAction("Index");
-        }
-
-        //DELETE ROUTES
-        [HttpGet("/tasks/{id}/delete")]
-        public ActionResult Delete(int id)
-        {
-            Task task = Task.Find(id);
-            return View(task);
-        }
-
-        [HttpPost("/tasks/{id}/delete")]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Task thisTask = Task.Find(id);
-            thisTask.Delete();
-            return View("DeleteConfirmed", thisTask);
-        }
-
-
-
-
     }
 }
---
+++ MySQLCore/Controllers/OLD_HomeController.cs
@@ -0,0 +1,77 @@
+// using System.Collections.Generic;
+// using Microsoft.AspNetCore.Mvc;
+// using MySQLCore.Models;
+// using System;
+
+// namespace MySQLCore.Controllers
+// {
+//     public class HomeController : Controller
+//     {
+
+// // HOME AND CREATE ROUTES
+
+//         [HttpGet("/")]
+//         public ActionResult Index()
+//         {
+//             List<Task> allTasks = Task.GetAll();
+//             List<Category> allCategories = Category.GetAll();
+//             Dictionary<string, object> model = new Dictionary<string, object>() {};
+//             model.Add("tasks", allTasks);
+//             model.Add("categories", allCategories);
+//             return View(model);
+//         }
+
+//         [HttpPost("/tasks/add")]
+//         public ActionResult CreateTask()
+//         {
+//             Task newTask = new Task(Request.Form["taskname"], Int32.Parse(Request.Form["taskcategory"]));
+//             newTask.Save();
+//             return RedirectToAction("Index");
+//         }
+
+//         [HttpPost("/category/add")]
+//         public ActionResult CreateCategory()
+//         {
+//             Category newCategory = new Category(Request.Form["categoryname"]);
+//             newCategory.Save();
+//             return RedirectToAction("Index");
+//         }
+
+
+//         //EDIT ROUTES
+//         [HttpGet("/tasks/{id}/edit")]
+//         public ActionResult EditTask(int id)
+//         {
+//             Task thisTask = Task.Find(id);
+//             return View(thisTask);
+//         }
+
+//         [HttpPost("/tasks/{id}/edit")]
+//         public ActionResult EditTaskConfirm(int id)
+//         {
+//             Task thisTask = Task.Find(id);
+//             thisTask.UpdateDescription(Request.Form["newname"]);
+//             return RedirectToAction("Index");
+//         }
+
+//         //DELETE ROUTES
+//         [HttpGet("/tasks/{id}/delete")]
+//         public ActionResult Delete(int id)
+//         {
+//             Task task = Task.Find(id);
+//             return View(task);
+//         }
+
+//         [HttpPost("/tasks/{id}/delete")]
+//         public ActionResult DeleteConfirmed(int id)
+//         {
+//             Task thisTask = Task.Find(id);
+//             thisTask.Delete();
+//             return View("DeleteConfirmed", thisTask);
+//         }
+
+
+
+
+//     }
+// }
